3d3aebc3a7226c50302e46730740e040e3fd88ed
zhangyunan1994 <zhangyunan@91jinrong.com>
2020-08-02 17:27:40 +0800

    添加首页

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container>
+      <el-header>
+        <el-row>
+          <el-col :span="8">
+            <div class='title'>Kola</div>
+          </el-col>
+          <el-col :span="16" style="text-align: right; padding-right: 30px;">
+            <el-button plain size="medium">登录</el-button>
+          </el-col>
+      </el-row></el-header>
+      <el-main>Main</el-main>
+      <el-footer>&copy; 2020 双鬼带单</el-footer>
+    </el-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+};
 </script>
+
+<style scope="this api replaced by slot-scope in 2.5.0+">
+.home {
+  height: 100%;
+}
+
+.title {
+  background-color: #1ec198;
+  width: 150px;
+  padding-left: 30px;
+}
+
+.el-container {
+  height: 100%;
+}
+
+.el-header {
+  height: 60px;
+  background-color: white;
+  line-height: 60px;
+  padding: 0!important;
+}
+
+.el-header > span,
+.el-header .el-dropdown {
+  font-size: 18px;
+}
+
+.el-footer {
+  background-color: #252d36;
+  color: #ffc852;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-main {
+  color: #333;
+  text-align: center;
+}
+</style>
